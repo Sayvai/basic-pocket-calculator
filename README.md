aec147f428e29db1c47f33db0b790ec46a0a8d94
Sayvai Tosu <sayvai@hotmail.com>
2022-07-16 20:36:49 +0100

    (chore) Integrate Cypress E2E Testing
    Integrates the Cypress 10 tool as the choice to implement and run E2E tests against.
    
    Comprehensive suite of tests written.
    
    Added helper NPM scripts to easily run E2E tests in two modes; Interactive (open) or Headless (run).
    
    Also added Cypress texts and running instructions to Readme file.

--- README.md
+++ README.md
@@ -14,35 +14,51 @@ The environment with which this project was implemented on:
 - and initially scaffolded from create-react-app `5.0.0`
 
 ## Running the Project
-
+---
 To run the project locally, run
 
 ```bash
   npm start
 ```
 
-## Running the Tests
-
+## Running the Unit Tests
+---
 To run unit tests on this project, run
 
 ```bash
   npm test
 ```
 
-## Browsers
+## Running the E2E Tests (Cypress)
+---
+⚠️ As a pre-requisite, the main project application must already be running, before attempting to run the E2E tests.
 
+To run the E2E tests interacively, run
+
+```bash
+`npm run e2e` - run the e2e tests interactively
+```
+
+To run the E2E tests headlessly, run
+
+```bash
+`npm run e2e:headless` - run the e2e tests headlessly
+```
+
+## Browsers
+---
 The following browsers were successfully tested on:
 
 - Chrome `98.0.4758.102`
 - Firefox `96.0.2`
 - Safari `15.2`
 ## Demo
-
+---
 Here is a GIF demonstrating of what to expect when you run the project, and a glimpse of the interactions with the web-based pocket calculator.
 
 ![GIF: Basic Pocket Calculator](https://user-images.githubusercontent.com/7581546/153953703-643e4513-ea07-4972-bab0-405a05c53cca.gif)
 
 ## Future Additions For Consideration
-
+---
 - Implement [Redux](https://redux.js.org/) to centrally and globally manage the basic state
-- Implement a functional / E2E testing tool (eg, [Cypress](https://www.cypress.io/) or [Playwright](https://playwright.dev/))
+- Implement the new Cypress feature; **Component Testing**. Further [reading](https://www.cypress.io/blog/2022/06/01/cypress-10-release/), or watch the [video](https://go.cypress.io/component-testing-recording) demonstration (~1 hour).
---
+++ cypress/e2e/calculator.cy.ts
@@ -0,0 +1,121 @@
+describe('Calculator', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  })
+
+  enum Selector {
+    BrandName = '[data-testid="component-branding"] h2',
+    ProductName = '[data-testid="component-branding"] h3',
+    OutputScreenInput = '[data-testid="component-screen"] input',
+    CalculationButtons = '[data-testid="component-buttons"] button',
+    ButtonClear = '[data-testid="Clear"]',
+    ButtonDivide = '[data-testid="Divide"]',
+    ButtonMultiply = '[data-testid="Multiply"]',
+    ButtonSubtract = '[data-testid="Subtract"]',
+    ButtonAdd = '[data-testid="Add"]',
+    ButtonEqual = '[data-testid="Equal"]',
+    ButtonZero = '[data-testid="Zero"]',
+    ButtonOne = '[data-testid="One"]',
+    ButtonTwo = '[data-testid="Two"]',
+    ButtonThree = '[data-testid="Three"]',
+    ButtonFour = '[data-testid="Four"]',
+    ButtonFive = '[data-testid="Five"]',
+    ButtonSix = '[data-testid="Six"]',
+    ButtonSeven = '[data-testid="Seven"]',
+    ButtonEight = '[data-testid="Eight"]',
+    ButtonNine = '[data-testid="Nine"]',
+  }
+
+  it(`should render the main default calculator elements:
+      - the main calculator UI
+      - The (configurable) brand text
+      - product name "Pocket Calculator"
+      - output screen
+      - calculation buttons`, () => {
+    cy.get(Selector.BrandName).should('be.visible');
+    cy.get(Selector.ProductName).should('be.visible');
+    cy.get(Selector.OutputScreenInput).should('be.visible');
+    cy.get(Selector.ButtonClear).should('be.visible');
+    cy.get(Selector.ButtonMultiply).should('be.visible');
+    cy.get(Selector.ButtonDivide).should('be.visible');
+    cy.get(Selector.ButtonSubtract).should('be.visible');
+    cy.get(Selector.ButtonAdd).should('be.visible');
+    cy.get(Selector.ButtonEqual).should('be.visible');
+    cy.get(Selector.ButtonOne).should('be.visible');
+    cy.get(Selector.ButtonTwo).should('be.visible');
+    cy.get(Selector.ButtonThree).should('be.visible');
+    cy.get(Selector.ButtonFour).should('be.visible');
+    cy.get(Selector.ButtonFive).should('be.visible');
+    cy.get(Selector.ButtonSix).should('be.visible');
+    cy.get(Selector.ButtonSeven).should('be.visible');
+    cy.get(Selector.ButtonEight).should('be.visible');
+    cy.get(Selector.ButtonNine).should('be.visible');
+  })
+
+  it(`should be able to perform a correct calculation: 1+1=2`, () => {
+    cy.get(Selector.ButtonOne).click();
+    cy.get(Selector.ButtonAdd).click();
+    cy.get(Selector.ButtonOne).click();
+    cy.get(Selector.ButtonEqual).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', 2);
+  })
+
+  it(`should be able to perform a correct calculation: 10-4=6`, () => {
+    cy.get(Selector.ButtonOne).click();
+    cy.get(Selector.ButtonZero).click();
+    cy.get(Selector.ButtonSubtract).click();
+    cy.get(Selector.ButtonFour).click();
+    cy.get(Selector.ButtonEqual).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', 6);
+  })
+
+  it(`should be able to perform a correct calculation: 8/2=4`, () => {
+    cy.get(Selector.ButtonEight).click();
+    cy.get(Selector.ButtonDivide).click();
+    cy.get(Selector.ButtonTwo).click();
+    cy.get(Selector.ButtonEqual).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', 4);
+  })
+
+  it(`should be able to perform a correct calculation: 6*9=54`, () => {
+    cy.get(Selector.ButtonSix).click();
+    cy.get(Selector.ButtonMultiply).click();
+    cy.get(Selector.ButtonNine).click();
+    cy.get(Selector.ButtonEqual).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', 54);
+  })
+
+  it(`should be able to perform a correct calculation: 6*10/2-5+1=26 (BODMAS)`, () => {
+    cy.get(Selector.ButtonSix).click();
+    cy.get(Selector.ButtonMultiply).click();
+    cy.get(Selector.ButtonOne).click();
+    cy.get(Selector.ButtonZero).click();
+    cy.get(Selector.ButtonDivide).click();
+    cy.get(Selector.ButtonTwo).click();
+    cy.get(Selector.ButtonSubtract).click();
+    cy.get(Selector.ButtonFive).click();
+    cy.get(Selector.ButtonAdd).click();
+    cy.get(Selector.ButtonOne).click();
+    cy.get(Selector.ButtonEqual).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', 26);
+  })
+
+  it('should be able to clear the screen output', () => {
+    cy.get(Selector.ButtonSeven).click();
+    cy.get(Selector.ButtonAdd).click();
+    cy.get(Selector.ButtonThree).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', '7+3');
+
+    cy.get(Selector.ButtonClear).click();
+
+    cy.get(Selector.OutputScreenInput).should('have.value', '');
+  })
+})
+
+export { }
